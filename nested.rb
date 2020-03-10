554fe99e09983dd7e0683e4454bebbbf4b68e8b4
Sajid Anjum <sajid1760@gmail.com>
2020-03-10 21:50:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,6 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:known_for][:languages][0]
 end
 
 def adding_matz
@@ -81,7 +82,7 @@ def adding_matz
         }
      }
 
-    
+    programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby",:languages => ["LISP","C"]}
 end
 
 def changing_alan
